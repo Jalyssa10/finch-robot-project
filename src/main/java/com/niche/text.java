1024a23d25db94aa1e4ec769e46cde6ac8846a8b
Jalyssa10 <jdelatorr024@mymail.lausd.net>
2025-06-04 12:26:23 -0700

    forked from lina

---
+++ src/main/java/com/niche/text.java
@@ -0,0 +1,14 @@
+package com.niche;
+
+public class Test{
+    public statis void main (String[] args){
+        System.out.print1n(x: "Hello");
+        //lalalallala
+
+        int[] scores = new int[20];
+        for (int i : scores){
+            System.out.print1n(i);
+        System.out.print1n(Nick: "Arigato")
+        }
+    }
+}
